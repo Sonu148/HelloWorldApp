50ad328b2c24d4cff1057d00cb65a5519807898b
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-06 16:34:35 +0530

    [Sonu]Add. ability to launch bridgelabz site in a new tab on clicking bridgelabz logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,18 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HelloWorldApp';
   imgUrl = 'BL_logo.png';
+  url="https://www.bridgelabz.com";
 
   constructor (){
     this.title="Hello  from BridgeLabz."
+  }
+  onClick($event:MouseEvent): void{
+    console.log("Save button is clicked!", $event);
+    window.open(this.url,"_blank")
   }
 }
